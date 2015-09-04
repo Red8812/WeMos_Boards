51a4e77f07521970c0ba99b84680c8f4ae3b7074
wemos <11569115@qq.com>
2015-09-04 12:17:19 +0800

    add

--- Makefile
+++ Makefile
@@ -6,12 +6,12 @@ file_lgt = wemos-lgt-$(ver_lgt).tar.gz
 .PHONY: all esp8266 lgt clean
 esp8266:
 	tar -zcvf $(file_esp8266) ESP8266
-	#7za.exe a -ttar -so wemos-esp8266-$(ver_esp8266) ESP8266/ | 7za.exe a -tgzip -si $(file_esp8266)
+
 lgt:
 	tar -zcvf $(file_lgt) LGT
 
 all:esp8266 lgt
-	python do_json.py $(file_esp8266) $(ver_esp8266) $(file_lgt) $(ver_lgt)
+	python do_json_cn.py $(file_esp8266) $(ver_esp8266) $(file_lgt) $(ver_lgt)
 	mv *.gz releases/
 clean:
 	rm -rf $(file_esp8266) $(file_lgt)
--- do_json.py
+++ do_json.py
@@ -4,13 +4,8 @@ import sys
 import os
 import hashlib
 import json
-
-f=open('temp.json','r')
-f_tmp=f.read()#get json_temp
-f.close()
-
-parsed_json = json.loads(f_tmp)
-j= parsed_json['packages']
+from oss.oss_api import *
+from oss.oss_xml_handler import *
 
 file_esp8266=sys.argv[1]
 ver_esp8266=sys.argv[2]
@@ -38,15 +33,23 @@ f.close()
 
 size_lgt = os.path.getsize(file_lgt)
 print size_lgt
+
+#do temp.json
+f=open('temp.json','r')
+f_tmp=f.read()#get json_temp
+f.close()
+
+parsed_json = json.loads(f_tmp)
+j= parsed_json['packages']
 #WeMos WiFi Boards
 j[0]['platforms'][0]['version']=ver_esp8266
-j[0]['platforms'][0]['url']=file_esp8266
+j[0]['platforms'][0]['url']="https://raw.githubusercontent.com/wemos/WeMos_Boards/master/releases/"+file_esp8266
 j[0]['platforms'][0]['archiveFileName']=file_esp8266
 j[0]['platforms'][0]['checksum']="SHA-256:"+sha
 j[0]['platforms'][0]['size']=str(size)
 #WeMos LGT Boards
 j[0]['platforms'][1]['version']=ver_lgt
-j[0]['platforms'][1]['url']=file_lgt
+j[0]['platforms'][1]['url']="https://raw.githubusercontent.com/wemos/WeMos_Boards/master/releases/"+file_lgt
 j[0]['platforms'][1]['archiveFileName']=file_lgt
 j[0]['platforms'][1]['checksum']="SHA-256:"+sha_lgt
 j[0]['platforms'][1]['size']=str(size_lgt)
@@ -55,4 +58,6 @@ j[0]['platforms'][1]['size']=str(size_lgt)
 f = open('package_wemos.cc_index.json', 'w')
 f.write(json.dumps(parsed_json))
 f.close()
+###
+
 
